f0a46087cf2dc22da1e4cc3cdff9a47fb6898771
Robert Snyder <r.snyder49@gmail.com>
2018-11-07 20:35:29 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,7 @@
 class PigLatinizer
   
-  def piglatinize(input_str)
-    x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+  def piglatinize(str)
+    x = (str.split(" ").length == 1) ? piglatinize_word(str) : piglatinize_sentence(str)
     puts x
     x
   end
