490a43879d3de5588432a96cbd2f4717daa976ac
Robert Snyder <r.snyder49@gmail.com>
2018-11-07 20:18:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :user_input
+  end 
+  
+  post '/piglatinize' do 
+    @user_input = params[:input]
+    @piglatin = PigLatinizer.new
+    erb :piglatinize
+  end 
+  
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,30 @@
+class PigLatinizer
+  
+  def piglatinize(word)
+    if %w[and an in].include?(word) 
+      word 
+    end
+    
+    letters = word.split("")
+    letters.keep_if {|letter| letter != "."}
+    if letters.size > 1
+      until vowel?(letters[0]) 
+        letters << letters.shift
+      end
+      letters  << "ay"
+    end
+    letters.join
+  end
+  
+  def to_pig_latin(text)
+    words = text.split(" ")
+    words.map! {|word| piglatinize(word)}
+    words.join(" ")
+  end
+  
+  def vowel?(letter)
+    letter.downcase
+    letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
+  end
+    
+ end 
