4c653b1dbab80bec23d57b068eb3c0e9256cb1b5
Robert Snyder <r.snyder49@gmail.com>
2018-11-07 20:34:57 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,27 +5,26 @@ class PigLatinizer
     puts x
     x
   end
-  # private
-   def consonant?(char)
-    !char.match(/[aAeEiIoOuU]/)
+  
+  def consonant?(letter)
+    !letter.match(/[aAeEiIoOuU]/)
   end
-   def piglatinize_word(word)
-    # word starts with vowel
+  
+  def piglatinize_word(word)
     if !consonant?(word[0])
       word = word + "w"
-    # word starts with 3 consonants
     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
       word = word.slice(3..-1) + word.slice(0,3)
-    # word starts with 2 consonants
     elsif consonant?(word[0]) && consonant?(word[1])
       word = word.slice(2..-1) + word.slice(0,2)
-    # word starts with 1 consonant
     else
       word = word.slice(1..-1) + word.slice(0)
     end
     word << "ay"
   end
-   def piglatinize_sentence(sentence)
+  
+  def piglatinize_sentence(sentence)
     sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
   end
+  
  end 
